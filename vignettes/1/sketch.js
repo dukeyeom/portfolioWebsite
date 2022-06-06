6f7722567da66f85cddbb97ff2c71d9a6efcafb6
dukeyeom <dukedyeom@gmail.com>
2022-06-05 20:28:39 -0400

    added local copy of p5.js

--- vignettes/1/sketch.js
+++ vignettes/1/sketch.js
@@ -1,13 +1,8 @@
 function setup() {
     createCanvas(400, 400);
-}
-
-function draw() {
-    if (mouseIfPressed) {
-        fill(0);
-    }
-    else {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-}
+  }
+  
+  function draw() {
+    background(220);
+    ellipse(50,50,80,80);
+  }
